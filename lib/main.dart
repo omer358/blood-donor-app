b0f7ac9247fd7953098a1c9672144cfba35fbca3
omer358 <omermaki358@gmail.com>
2024-09-11 15:13:39 +0300

    save user signin state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:blood_donor/presentation/screens/home.dart';
 import 'package:blood_donor/presentation/screens/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,7 +8,6 @@ import 'package:get/get.dart';
 import 'firebase_options.dart';
 
 void main() async {
-
   // Ensure that widgets are initialized
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Blood Donor App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -30,7 +31,28 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         colorScheme: const ColorScheme.dark(),
       ),
-      home: LoginScreen(),
+      home: const AuthWrapper(), // Use the AuthWrapper to handle navigation
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // If the snapshot has user data, the user is logged in
+        if (snapshot.hasData) {
+          // If user is signed in, navigate to HomeScreen
+          return const HomeScreen();
+        } else {
+          // If the user is not signed in, navigate to LoginScreen
+          return LoginScreen();
+        }
+      },
     );
   }
 }
